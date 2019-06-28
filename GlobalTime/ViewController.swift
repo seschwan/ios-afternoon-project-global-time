7da2f204914e05ebd02fa540f31f6d567795cf45
Seschwan <seschwan@Seschwans-MacBook-Pro.local>
2019-06-27 20:22:21 -0600

    Project Start

---
+++ GlobalTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GlobalTime
+//
+//  Created by Seschwan on 6/27/19.
+//  Copyright © 2019 Seschwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
